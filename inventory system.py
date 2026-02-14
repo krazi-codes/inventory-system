139e19bf9988ba80554921af0d34925a55262057
Tuan Vo <Tuanvo7463@gmail.com>
2026-02-14 06:17:26 -0500

    Update inventory system.py
    completed add
    added rightclick menu to inventory tree
    need to add functionallity to rightclick menu

--- inventory system.py
+++ inventory system.py
@@ -7,6 +7,7 @@ root.geometry('{}x{}'.format(1280, 720))
 root.title('Inventory Managment')
 root.config(bg='red')
 
+
 #create the frames for the widgets
 mainframe = tk.Frame(root,background='#DADBD5')
 mainframe.pack(fill=tk.BOTH,expand=True)
@@ -36,6 +37,7 @@ company = Label(topbar,text='The Company',bg='#1b263b',font=('Arial',20),foregro
 company.pack(side=tk.LEFT,padx=(0,0))
 
 
+
 #Create the inventory system
 inventory_scrollbar = tk.Scrollbar(margin_bar)
 inventory_scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
@@ -54,6 +56,23 @@ inventory_scrollbar.config(command = inventory_tree.yview)
 ttk.Style().configure("Treeview", background="#E0E1DD", foreground="black")
 inventory_tree.pack_propagate(False)
 
+m = tk.Menu(inventory_frame,tearoff=0)
+m.add_command(label='Edit')
+m.add_command(label='Delete')
+
+
+def rclick(event):
+    try:
+        click = inventory_tree.identify_row(event.y)
+        inventory_tree.selection_set(click)
+        m.tk_popup(event.x_root,event.y_root)
+    finally:
+        m.grab_release()
+
+inventory_tree.bind("<Button-3>",lambda event : rclick(event))
+
+
+
 #Create the log tree
 log_scrollbar = tk.Scrollbar(audit_log_frame)
 log_scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
@@ -66,6 +85,9 @@ log_tree.heading('1',text='Stock Change')
 log_tree.pack(fill=tk.BOTH,expand=True,padx=(0,180),side=tk.BOTTOM)
 log_scrollbar.config(command = log_tree.yview)
 ttk.Style().configure("Treeview", background="#E0E1DD", foreground="black")
+
+
+
 
 #button class
 class flatbutton:
@@ -94,17 +116,38 @@ def open_log_frame():
 
 def add_popup():
     pop = tk.Toplevel(root)
-    pop.geometry('400x200')
-
+    pop.geometry("400x200")
     product = tk.StringVar()
     product_id = tk.StringVar()
     cost = tk.StringVar()
     amount = tk.StringVar()
+    
+    class textentry:
+        def __init__(self,name,strvar):
+            xpad = 70
+            framec = tk.Frame(pop)
+            framec.pack(fill=tk.X,expand=True)
+            label = tk.Label(framec,text=name)
+            label.pack(side=tk.LEFT,padx=(xpad,0))
+            enter = tk.Entry(framec,textvariable=strvar)
+            enter.pack(side=tk.RIGHT,padx=(0,xpad))
+    
+    def inventory_insert():
+        c = cost.get()
+        a = amount.get()
+        total_sum = float(a)*float(c)
+        inventory_tree.insert('','end',text=f'{product.get()}',values=(f'{product_id.get()}',f'{c}',f'{a}',f'{total_sum:.2f}'))
+        return 
 
-    for x in range(len(['Product','Product_ID','Cost','Amount'])):
-        string = Label(pop,text=x)
-        string.grid()
-        
+    textentry('Product:',product)
+    textentry('Product_ID:',product_id)
+    textentry('Cost:',cost)
+    textentry('Amount:',amount)
+    
+    bframe = tk.Frame(pop)
+    bframe.pack(fill=tk.X,expand=True)
+    add_button = tk.Button(bframe,text='Add',width=4,height=1,font=('Arial',13),command=inventory_insert)
+    add_button.pack(fill=tk.Y)
     return
 
 homepage_Button = flatbutton(sidebar,5,tk.X,(4,4),(4,5),'#acb7c3','#5e8ca7','Homepage', open_front_frame)
@@ -114,8 +157,12 @@ log_button = flatbutton(sidebar,5,tk.X,(4,4),(4,5),'#acb7c3','#5e8ca7','Audit Lo
 add_item = tk.Button(margin_bar,width=3,height=1,text='+',font=('Arial',30),command=add_popup)
 add_item.pack(side=tk.BOTTOM,padx=30,pady=40)
 
+
+
+
+
 #inventory system filler
-for x in range(1000):
-    inventory_tree.insert('', 'end', text=f'{x+1} Example product', values=('value 1','value 2','value 3'))
+for x in range(3):
+    inventory_tree.insert('', 'end', text=f'{x+1} Example product', values=('value 1','value 2','value 3','value 4'))
 
 root.mainloop()
