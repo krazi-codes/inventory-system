75faadc22d9d4c5073325dc7ef6d191c36d7cc7f
Tuan Vo <Tuanvo7463@gmail.com>
2026-02-14 12:12:24 -0500

    Update inventory system.py

--- inventory system.py
+++ inventory system.py
@@ -37,7 +37,6 @@ company = Label(topbar,text='The Company',bg='#1b263b',font=('Arial',20),foregro
 company.pack(side=tk.LEFT,padx=(0,0))
 
 
-
 #Create the inventory system
 inventory_scrollbar = tk.Scrollbar(margin_bar)
 inventory_scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
@@ -56,9 +55,25 @@ inventory_scrollbar.config(command = inventory_tree.yview)
 ttk.Style().configure("Treeview", background="#E0E1DD", foreground="black")
 inventory_tree.pack_propagate(False)
 
+
+
+
+def tree_delete():
+    iid = inventory_tree.selection()[0]
+    inventory_tree.delete(iid)
+    return
+
+def tree_edit():
+    for item in inventory_tree.selection():
+        item = inventory_tree.item(item)
+        val = item['values']
+        edit_popup(val)
+    return
+
+
 m = tk.Menu(inventory_frame,tearoff=0)
-m.add_command(label='Edit')
-m.add_command(label='Delete')
+m.add_command(label='Edit',command= tree_edit)
+m.add_command(label='Delete',command= tree_delete)
 
 
 def rclick(event):
@@ -72,7 +87,6 @@ def rclick(event):
 inventory_tree.bind("<Button-3>",lambda event : rclick(event))
 
 
-
 #Create the log tree
 log_scrollbar = tk.Scrollbar(audit_log_frame)
 log_scrollbar.pack(side=tk.RIGHT,fill=tk.Y)
@@ -82,11 +96,9 @@ log_tree['columns'] =list(range(2))
 log_tree.heading('#0',text='Product')
 log_tree.heading('0',text='Product ID')
 log_tree.heading('1',text='Stock Change')
-log_tree.pack(fill=tk.BOTH,expand=True,padx=(0,180),side=tk.BOTTOM)
+log_tree.pack(fill=tk.BOTH,expand=True,side=tk.BOTTOM)
 log_scrollbar.config(command = log_tree.yview)
 ttk.Style().configure("Treeview", background="#E0E1DD", foreground="black")
-
-
 
 
 #button class
@@ -114,7 +126,7 @@ def open_log_frame():
     audit_log_frame.pack(side=tk.BOTTOM,expand=True,fill=tk.BOTH)
     return
 
-def add_popup():
+def edit_popup(text):
     pop = tk.Toplevel(root)
     pop.geometry("400x200")
     product = tk.StringVar()
@@ -123,15 +135,54 @@ def add_popup():
     amount = tk.StringVar()
     
     class textentry:
-        def __init__(self,name,strvar):
+        def __init__(self,name,strvar,text):
             xpad = 70
             framec = tk.Frame(pop)
             framec.pack(fill=tk.X,expand=True)
             label = tk.Label(framec,text=name)
             label.pack(side=tk.LEFT,padx=(xpad,0))
             enter = tk.Entry(framec,textvariable=strvar)
+            enter.insert(0,text)
             enter.pack(side=tk.RIGHT,padx=(0,xpad))
+
+    def inventory_insert():
+        c = cost.get()
+        a = amount.get()
+        total_sum = float(a)*float(c)
+        inventory_tree.insert('','end',text=f'{product.get()}',values=(f'{product_id.get()}',f'{c}',f'{a}',f'{total_sum:.2f}'))
+        return 
     
+    textentry('Product:',product,text[0])
+    textentry('Product_ID:',product_id,text[1])
+    textentry('Cost:',cost,text[2])
+    textentry('Amount:',amount,text[3])
+    
+    bframe = tk.Frame(pop)
+    bframe.pack(fill=tk.X,expand=True)
+    add_button = tk.Button(bframe,text='Add',width=4,height=1,font=('Arial',13),command=inventory_insert)
+    add_button.pack(fill=tk.Y)
+    return
+
+
+def add_popup():
+    pop = tk.Toplevel(root)
+    pop.geometry("400x200")
+    product = tk.StringVar()
+    product_id = tk.StringVar()
+    cost = tk.StringVar()
+    amount = tk.StringVar()
+
+    class textentry:
+        def __init__(self,name,strvar,text):
+            xpad = 70
+            framec = tk.Frame(pop)
+            framec.pack(fill=tk.X,expand=True)
+            label = tk.Label(framec,text=name)
+            label.pack(side=tk.LEFT,padx=(xpad,0))
+            enter = tk.Entry(framec,textvariable=strvar,show=text)
+            enter.pack(side=tk.RIGHT,padx=(0,xpad))
+
+
     def inventory_insert():
         c = cost.get()
         a = amount.get()
@@ -139,10 +190,10 @@ def add_popup():
         inventory_tree.insert('','end',text=f'{product.get()}',values=(f'{product_id.get()}',f'{c}',f'{a}',f'{total_sum:.2f}'))
         return 
 
-    textentry('Product:',product)
-    textentry('Product_ID:',product_id)
-    textentry('Cost:',cost)
-    textentry('Amount:',amount)
+    textentry('Product:',product,None)
+    textentry('Product_ID:',product_id,None)
+    textentry('Cost:',cost,None)
+    textentry('Amount:',amount,None)
     
     bframe = tk.Frame(pop)
     bframe.pack(fill=tk.X,expand=True)
@@ -153,11 +204,8 @@ def add_popup():
 homepage_Button = flatbutton(sidebar,5,tk.X,(4,4),(4,5),'#acb7c3','#5e8ca7','Homepage', open_front_frame)
 inventory_Button = flatbutton(sidebar,5,tk.X,(4,4),(4,5),'#acb7c3','#5e8ca7','Inventory System', open_inventory_frame)
 log_button = flatbutton(sidebar,5,tk.X,(4,4),(4,5),'#acb7c3','#5e8ca7','Audit Log', open_log_frame)
-
 add_item = tk.Button(margin_bar,width=3,height=1,text='+',font=('Arial',30),command=add_popup)
 add_item.pack(side=tk.BOTTOM,padx=30,pady=40)
-
-
 
 
 
